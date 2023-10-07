bf3b79d734d7c0b3eb0193d2a4ee77b84c0be637
ErickJMenezes <erickmenezes.dev@gmail.com>
2023-10-07 16:02:49 -0300

    feat: basic lib scaffolding and registration.

--- src/Commands/ModuleMakeLibCommand.php
+++ src/Commands/ModuleMakeLibCommand.php
@@ -10,6 +10,8 @@ use ErickJMenezes\Modularavel\Scaffolding\Tree;
 use ErickJMenezes\Modularavel\Scaffolding\Generator;
 use Illuminate\Console\Command;
 use ErickJMenezes\Modularavel\Scaffolding\Command as GeneratorCommand;
+use Illuminate\Support\Composer;
+use Illuminate\Support\Str;
 
 class ModuleMakeLibCommand extends Command
 {
@@ -18,14 +20,37 @@ class ModuleMakeLibCommand extends Command
 
     protected $description = 'Generates a new library';
 
-    public function handle(Generator $generator)
+    public function handle(Generator $generator, Composer $composer)
     {
         $baseDirectory = (string) config('modularavel.libraries_directory');
-        $libName = (string)$this->argument('name');
+        $libName = (string) $this->argument('name');
         $laravelVersion = InstalledVersions::getPrettyVersion('laravel/framework');
 
-        $this->info("Generating scaffold of \"$libName\"...");
+        $this->info("Scaffolding \"$libName\"...");
 
+        $this->scaffoldLibrary($generator, $libName, $laravelVersion, $baseDirectory);
+
+        $this->info('Generated files and folders.');
+
+        $this->info('Bootstrapping library...');
+
+        $this->bootstrappingLibrary($composer, $baseDirectory, $libName);
+
+        $this->info('Registering the new library...');
+
+        $this->registerLibrary($composer, $libName);
+
+        $this->info("Done.");
+        $this->newLine();
+    }
+
+    private function scaffoldLibrary(
+        Generator $generator,
+        string $libName,
+        ?string $laravelVersion,
+        string $baseDirectory
+    ): void
+    {
         $generator->generate(
             new Tree([
                 new Folder($libName, new Tree([
@@ -72,22 +97,41 @@ class ModuleMakeLibCommand extends Command
                         "--stability=stable",
                         "--autoload=src",
                         "--require=laravel/framework:^$laravelVersion",
-                        "--require=php:^8.2",
                         '--require-dev=pestphp/pest:^2.21',
                         '--no-interaction',
                     ]),
                     new File('.gitignore', "/vendor\n/node_modules\n.phpunit.result.cache\n"),
-                ]))
+                ])),
             ]),
             $baseDirectory,
         );
+    }
 
-        $this->info("The generation is now completed!");
-        $this->newLine();
-        $this->line("Now you must add the ");
-        $this->line("cd libs/$libName");
-        $this->line("composer install");
-        $this->line("./vendor/bin/pest init");
-        $this->newLine();
+    private function bootstrappingLibrary(
+        Composer $composer,
+        string $baseDirectory,
+        string $libName,
+    ): void
+    {
+        $composer->setWorkingPath($baseDirectory.DIRECTORY_SEPARATOR.$libName)
+            ->modify(function (array $file) use ($libName) {
+                $lib = Str::studly($libName);
+                $file['extra']['laravel']['providers'][] = "Libs\\{$lib}\\Providers\\{$lib}ServiceProvider";
+                return $file;
+            });
+    }
+
+    private function registerLibrary(Composer $composer, string $libName): void
+    {
+        $composer->setWorkingPath(base_path())
+            ->modify(function (array $file) use ($libName) {
+                $file['repositories'][] = [
+                    'type' => 'path',
+                    'url' => "libs/$libName",
+                    'symlink' => true,
+                ];
+                return $file;
+            });
+        $composer->requirePackages(["libs/$libName:@dev"], output: $this->getOutput());
     }
 }
